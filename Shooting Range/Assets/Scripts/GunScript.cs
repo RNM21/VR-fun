e67a06d89f262b6727e2535787e7cbaa026f662d
rnm510 <rnm510@users.noreply.github.com>
2020-09-24 14:47:09 -0400

    Shooting range
    Base shooting range game. I'll add more guns as time goes on

---
+++ Shooting Range/Assets/Scripts/GunScript.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    [RequireComponent(typeof(AudioSource))]
+
+    public class GunScript : MonoBehaviour
+    {
+        public bool singleFire = false;
+        public int speed = 40;
+        public float fireRate = 0.1f;
+        public GameObject bulletPrefab;
+        public Transform firePoint;
+        public int bulletsPerMagazine = 30;
+        public float timeToReload = 1.5f;
+        public float weaponDamage = 15; //How much damage should this weapon deal
+        public AudioClip fireAudio;
+        public AudioClip reloadAudio;
+
+        float nextFireTime = 0;
+        bool canFire = true;
+        int bulletsPerMagazineDefault = 0;
+        AudioSource audioSource;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            bulletsPerMagazineDefault = bulletsPerMagazine;
+            audioSource = GetComponent<AudioSource>();
+            audioSource.playOnAwake = false;
+            //Make sound 3D
+            audioSource.spatialBlend = 1f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+           
+        }
+
+        public void Fire()
+        {
+            if (canFire)
+            {
+                if (Time.time > nextFireTime)
+                {
+                    nextFireTime = Time.time + fireRate;
+
+                    if (bulletsPerMagazine > 0)
+                    {
+                        GameObject bulletObject = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+                        //bulletObject.GetComponent<Rigidbody>().velocity = speed * firePoint.forward;
+                        BulletScript bullet = bulletObject.GetComponent<BulletScript>();
+
+                        //Set bullet damage according to weapon damage value
+                        bullet.SetDamage(weaponDamage);
+
+                        bulletsPerMagazine--;
+                        audioSource.clip = fireAudio;
+                        audioSource.Play();
+                    }
+                    else
+                    {
+                        StartCoroutine(Reload());
+                    }
+                }
+            }
+        }
+
+        IEnumerator Reload()
+        {
+            canFire = false;
+
+            audioSource.clip = reloadAudio;
+            audioSource.Play();
+
+            yield return new WaitForSeconds(timeToReload);
+
+            bulletsPerMagazine = bulletsPerMagazineDefault;
+
+            canFire = true;
+        }
+
+        //Called from SC_WeaponManager
+        public void ActivateWeapon(bool activate)
+        {
+            StopAllCoroutines();
+            canFire = true;
+            gameObject.SetActive(activate);
+        }
+    }
+
